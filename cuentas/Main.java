489cae20e6d87be0b02255370e5d5323a3b4cde5
Tomas <tomaserobino@gmail.com>
2025-02-22 17:03:01 +0100

    extraccion de metodo

--- cuentas/Main.java
+++ cuentas/Main.java
@@ -3,9 +3,12 @@ package cuentas;
 public class Main {
 
     public static void main(String[] args) {
+        operativa_cuenta(1);
+    }
+
+    private static void operativa_cuenta(float cantidad) {
         CCuenta cuenta1;
         double saldoActual;
-
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
